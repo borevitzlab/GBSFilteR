ca46470159ea72254690ec14cfcc6ba6b85e0c30
Kevin Murray <k.d.murray.91@gmail.com>
2012-09-13 15:42:53 +1000

    separate parameters, renames files by parameters. Fix bugs

--- HapMapFilter.R
+++ HapMapFilter.R
@@ -1,6 +1,13 @@
 #change working directory
-setwd("/home/kevin/UniWork/BIOL3157/Assignments/3")
 setwd("./pel2b")
+
+
+### Set Cutoffs
+snp.cutoff = 78 # SNP must be present in this many samples 78
+sample.cutoff = 500 # Sample must have data on at least this many SNPS 500
+paralog.cutoff = 35 # If snps have more than this many heterozygotes 35
+
+
 
 ### Import data
 # Read TASSEL output which contains SNP data
@@ -11,19 +18,15 @@ g <- geno[,-(1:11)]
 
 ### Image raw data
 # Save (to png device)
-png(file="raw.png")
+tiff(file="raw.tiff", antialias="none")
 # Create colour map of genotype matrix
 # White cells are missing data
-# Green is 0 Homozygous minor allele
-# Brown is 1, Het
-# Blue-grey is 2 Homozygous major allele
 image(as.matrix(g), main="Raw Data (pel2b)", col=rainbow(3))
 dev.off() 
 
 ### Filter data
 # For each row (SNP) in the genotype, count how many samples have data on this snp
 snp.sample.counts <- apply(g, 1, function(x) sum(!is.na(x)))
-snp.cutoff = 78
 
 # Plot the number of samples each snp has
 plot(cumsum(table(snp.sample.counts)))
@@ -31,7 +34,7 @@ hist(snp.sample.counts,breaks=96)
 abline(v=snp.cutoff)
 
 # Save histogram which describes the cutoff point used
-png(file="snp_filtering_hist.png")
+png(file=paste(sample.cutoff, snp.cutoff, paralog.cutoff, "snp_filtering_hist.png"))
 hist(snp.sample.counts,breaks=96)
 abline(v=snp.cutoff)
 dev.off()
@@ -43,15 +46,15 @@ table(snp.sample.counts > snp.cutoff)
 g.snp <- g[snp.sample.counts > snp.cutoff,]
 
 
+
 ### Qualtity control samples
 # For each column (Samples) in the filterd SNP set, count how many SNPs this sample has
 sample.snp.counts<-apply(g.snp,2,function(x) sum(!is.na(x)))
-sample.cutoff = 500
 # Plot graphical summary of samples
 hist(sample.snp.counts, breaks=40, xlim=c(0,600))
 abline(v=sample.cutoff)
 
-png(file="sample_filtering_hist.png")
+png(file=paste(sample.cutoff, snp.cutoff, paralog.cutoff, "sample_filtering_hist.png"))
 hist(sample.snp.counts, breaks=40, xlim=c(0,600))
 abline(v=sample.cutoff)
 dev.off()
@@ -62,9 +65,10 @@ gg <- g.snp[,sample.snp.counts > sample.cutoff] #select top 80 samples
 # How much data is still missing?
 table(!is.na(gg))
 image(as.matrix(gg), col=rainbow(3))
-png(file="data_post_filtering.png")
+png(file=paste(sample.cutoff, snp.cutoff, paralog.cutoff, "data_post_filtering.png"))
 image(as.matrix(gg), col=rainbow(3))
 dev.off()
+
 
 
 ### Calculate major and minor allele encodings, and filter paralogs
@@ -85,6 +89,8 @@ g.minor[freq < tot,] <- gg[freq < tot,]
 min.allele.freq <- rowSums(g.minor, na.rm=T) / (rowSums(!is.na(g.minor)) *2)
 hist(min.allele.freq, breaks=20)
 
+
+
 ### Filter for paralogs
 # Excessive heterozygosity across many samples indicates probable paralogs
 image(g.minor==1, col=rainbow(3))
@@ -102,11 +108,21 @@ hets.per.snp <- rowSums(g.minor==1, na.rm=T)
 hist(hets.per.snp, breaks=20)
 
 # filter out paralogs
-paralogous.snps <- hets.per.snp >30
+paralogous.snps <- hets.per.snp > paralog.cutoff
 g.final <- g.minor[!paralogous.snps,]
 
 # minor allele frequency distribution
 final.min.allele.freq <- rowSums(g.final,na.rm=T)/ (2 * rowSums(!is.na(g.final)))
+hist(final.min.allele.freq, breaks=20)
+
+# How much data is still missing?
+table(!is.na(g.final))
+image(as.matrix(g.final), col=rainbow(3))
+png(file=paste(sample.cutoff, snp.cutoff, paralog.cutoff, "final_data.png"))
+image(as.matrix(g.final), col=rainbow(3))
+dev.off()
+
+
 
 ### Create name matrix. Do it down here, so we can apply this to the phylogenetic grouping
 # Names gets the name of each column in g.final (the name of the sample)
@@ -118,6 +134,8 @@ names.lst <- unlist(split.names)
 # Create names matrix. nrow splits list into matrix with 5 rows
 names.matrix <- matrix(names.lst, nrow=5)
 names <- names.matrix[5,]
+
+
 
 ### Edits name matrix
 # Substitute "A01" to "A1". "\\1" is a regex backreference to the first subexpression in parenthesis.
@@ -149,36 +167,61 @@ names.matrix <- rbind(
 names(g.final) <- paste(names.matrix[10,],names.matrix[7,],names.matrix[11,])
 
 
+
 ### Make tree, define colour groups
-tree <- hclust(as.dist(1-cor(g.minor,use = "pairwise.complete.obs")))
+tree <- hclust(as.dist(1-cor(g.final,use = "pairwise.complete.obs")))
 tree.groups <- cutree(tree, k=5)
 
 # Assigns each phylogenetic group a colour
 plot.col <- rainbow(max(as.numeric(tree.groups)))[as.numeric(tree.groups)]
 
-# better to polarize by ancetral state and use full freq specta ????
 
-pdf(file = "filtered.pel2b.pdf")
+
+### EXPORT FIGURES
+pdf(file = paste(sample.cutoff, snp.cutoff, paralog.cutoff, "filtered.pel2b.pdf"))
 # Minor Allele Frequency histogram of final data
-hist(final.min.allele.freq,breaks= 80, main="Minor Allele Frequencies")
+hist(final.min.allele.freq,breaks=50, main="Minor Allele Frequencies")
 # Genotype call image of final data
-image(as.matrix(g.final), main="Genotype Call Data", col=rainbow(3), ylab="Samples", xlab="SNPs")
+image(
+  as.matrix(g.final), 
+  main="Genotype Call Data", 
+  sub=paste("sample_cutoff=", sample.cutoff, "snp_cutoff=", snp.cutoff),
+  col=rainbow(3), 
+  ylab="Samples", 
+  xlab="SNPs"
+  )
 # Plot tree, including coloured boxes around different phylogenetic groups
-plot(tree, cex = 0.5)  #cex scales text by 0.5
-rect.hclust(tree,k=10, border=rainbow(max(as.numeric(tree.groups))), cluster=tree.groups)
+plot(
+  tree, 
+  cex = 0.5, #cex scales text by 0.5
+  sub=paste("sample_cutoff=", sample.cutoff, "snp_cutoff=", snp.cutoff),
+  xlab=""
+  )  
+rect.hclust(
+  tree, 
+  k=5, 
+  border=rainbow(max(as.numeric(tree.groups))), 
+  cluster=tree.groups
+  )
 dev.off()
 
-### Export final data.
-write.csv(g.final,file = "pel2b_FilteredGenotypes.csv")
 
+### Export final filtered data.
+write.csv(g.final,file = paste(sample.cutoff, snp.cutoff, paralog.cutoff, "pel2b_FilteredGenotypes.csv"))
 
 ### Export data to gps file
 # Creates a data.frame of each sample
-gps.file <- cbind(
+# Points are coloured based on cutree phylogenetic groups
+# Only samples which passed filtering are used
+gps.file <- data.frame(
   Names=names(g.final),
   color=plot.col,
   Lat=names.matrix[8,],
   Long=names.matrix[9,]
   )
 
-write.csv(file="pel2b_GPS.csv", gps.file)
+# Write gps data frame to csv
+write.csv(file=paste(sample.cutoff, snp.cutoff, paralog.cutoff, "pel2b_GPS.csv"), gps.file)
+dat <- as.numeric(table(!is.na(g.final)))
+table(!is.na(g))
+print(paste("Final data matrix has", dat[1], "pieces of missing data, and", dat[2], "pieces of data present"))
