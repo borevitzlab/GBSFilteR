80ae54d99ca6d7f83b6fe6a6a4d6fad789acfcf6
Kevin Murray <k.d.murray.91@gmail.com>
2012-11-01 12:15:22 +1100

    Commented and renamed tassel_hmp_to_tagseq.R to tassel_hmn_to_tagseq.R

--- tassel_hmn_to_tagseq.R
+++ tassel_hmn_to_tagseq.R
@@ -1,15 +1,15 @@
 args <- commandArgs(trailingOnly=T)
-#1: Tassel hmn.txt table
-#2: fasta csv
-#3: hmnfas tagseq output file
+# args[1] = "pel2b_crosscheck.hmn.txt"
+# args[2] = "pel2b_crosscheck.fas.csv"
+# args[3] = "pel2b_crosscheck"
 
-args[1] = "pel2b_crosscheck.hmn.txt"
-args[2] = "pel2b_crosscheck.fas.csv"
-args[3] = "pel2b_crosscheck.hmnfas.csv"
-
+### READ DATA
 tassel <- read.table(args[1], header=T)
 tassel.seqs <- read.csv(args[2])
+
+###MATCH ROWS
 seq.tp.match <- match(as.character(tassel.seqs$seq.names), tassel[,1])
 tassel[,1] <- tassel.seqs[seq.tp.match,3]
 
-write.csv(tassel, file=args[3])
+#write result
+write.csv(tassel, file=paste(args[3], "hmnfas.csv", sep="."))
