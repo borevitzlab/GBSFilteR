e92acad701c68f6c0c7467b77b362827c4f16026
Kevin Murray <k.d.murray.91@gmail.com>
2012-11-01 14:44:40 +1100

    crosscheck_samples.R now makes table()s

--- crosscheck_samples.R
+++ crosscheck_samples.R
@@ -1,8 +1,11 @@
 args <- commandArgs(trailingOnly=T)
-#1: Tassel tmn.txt table
-#2: fasta
 
-tassel <- read.csv
+pel2b <- read.csv("pel2b.filtered.csv")
+pel3 <- read.csv("pel3.filtered.csv")
+pel2bn3 <- read.csv("pel2bn3.filtered.csv")
 
-fasta <- read.csv(args[2])
-
+techrep.rows <- c()
+match(names(pel2b), names(pel3))
+table(!is.na(match(pel2b$tagseqs, pel3$tagseqs)))
+table(!is.na(match(pel2bn3$tagseqs, pel2b$tagseqs)))
+table(!is.na(match(pel2bn3$tagseqs, pel3$tagseqs)))
