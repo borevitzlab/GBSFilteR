efe8170aa1b2d84a1f7c01d48fb630b3b4c9414b
Kevin Murray <k.d.murray.91@gmail.com>
2012-11-01 17:50:18 +1100

    added phangorn and changed repos to use csiro's ones automatically

--- setup_libs.R
+++ setup_libs.R
@@ -1,5 +1,6 @@
 
 #Setup
-
-install.packages("seqinr")
+install.packages("seqinr",repos = "http://cran.csiro.au/")
 library(seqinr)
+install.packages("phangorn",repos = "http://cran.csiro.au/")
+library(phangorn)
