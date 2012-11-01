87cc13c0d1555d8d69b5e42f58d4162256f3269a
Kevin Murray <k.d.murray.91@gmail.com>
2012-11-01 18:55:48 +1100

    added names.matrix import to tassel_create_gps_table.R

--- tassel_create_gps_table.R
+++ tassel_create_gps_table.R
@@ -2,7 +2,8 @@
 args <- commandArgs(trailingOnly=T)
 # args[1] = "pel2b_crosscheck.filtered.csv"
 # args[2] = "pel2b_crosscheck.phylogroups.csv"
-# args[3] = "pel2b_crosscheck"
+# args[3] = "pel2b_crosscheck.names.csv"
+# args[4] = "pel2b_crosscheck"
 
 
 # Read genotype data created tassel_filter_hapmap.R
@@ -11,15 +12,16 @@ genotype <- genotype.seqs[,seq(2,length(genotype.seqs)-1)] # remove seqs for tre
 row.names(genotype) <- genotype.seqs$X # restore row names
 
 
-### Make tree, define colour groups
+### Define colour groups
 tree.groups.tab <- read.csv(args[2])
 tree.groups <- as.numeric(tree.groups.tab[,2])
 names(tree.groups) <- as.character(tree.groups.tab[,1])
 
-
 # Assigns each phylogenetic group a colour
 plot.col <- rainbow(max(as.numeric(tree.groups)))[as.numeric(tree.groups)]
 
+### Get names data
+names <- as.matrix(read.csv(args[3]))
 
 ### Export data to gps file
 # Creates a data.frame of each sample
@@ -33,4 +35,4 @@ gps.file <- data.frame(
 )
 
 # Write gps data frame to csv
-write.csv(gps.file,file = paste(args[3],"gps.csv", sep="."))
+write.csv(gps.file,file = paste(args[4],"gps.csv", sep="."))
