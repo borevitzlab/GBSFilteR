3cb2116565a4844842e53651130ebe28d8d0d975
Kevin Murray <k.d.murray.91@gmail.com>
2012-11-01 12:16:51 +1100

    got tassel_create_tree.R working, now exports phylogroups

--- tassel_create_tree.R
+++ tassel_create_tree.R
@@ -1,22 +1,25 @@
 #change working directory
 args <- commandArgs(trailingOnly=T)
-#1: hmnfas file (hmn file with tag sequences)
-#2: output file prefix
-
 args[1] = "pel2b_crosscheck.filtered.csv"
 args[2] = "pel2b_crosscheck"
 
+### READ DATA
 # Read genotype data created tassel_filter_hapmap.R
 genotype.seqs <- read.csv(args[1])
-genotype <- genotype.seqs[,seq(2,length(genotype.seqs)-1)] # remove seqs for tree generation
+# remove tag seqs for tree generation
+genotype <- genotype.seqs[,seq(2,length(genotype.seqs)-1)] 
 row.names(genotype) <- genotype.seqs$X # restore row names
 
-### Make tree, define colour groups
+
+### MAKE TREE
+# Make tree, define colour groups
 tree <- hclust(as.dist(1-cor(genotype,use = "pairwise.complete.obs")))
 tree.groups <- cutree(tree, k=5)
+write.csv(tree.groups, file=paste(args[2], "phylogroups.csv", sep="."))
 
 # Assigns each phylogenetic group a colour
 plot.col <- rainbow(max(as.numeric(tree.groups)))[as.numeric(tree.groups)]
+
 
 ### EXPORT FIGURES
 pdf(file=paste(args[2], "tree.pdf", sep="."))
@@ -26,7 +29,7 @@ plot(
   cex = 0.7, #cex scales text by 0.5
   sub=paste("sample_cutoff=", sample.cutoff, "snp_cutoff=", snp.cutoff),
   xlab=""
-)  
+)
 rect.hclust(
   tree, 
   k=5, 
